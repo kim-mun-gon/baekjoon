0654e65dcaaaeb3dd75043c6d8f4b3b5d68c89f2
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2021-01-07 16:04:06 +0900

    10871번

---
+++ X보다 작은 수.c
@@ -0,0 +1,19 @@
+#include<stdio.h>
+
+int main(void)
+{
+	int N, X;
+	int arr[10000];
+	scanf("%d %d", &N, &X);
+	
+	for(int i = 0; i < N; i++)
+	{
+		scanf("%d", &arr[i]);
+		if(arr[i] < X)
+		{
+			printf("%d ", arr[i]);
+		}
+	}
+	return 0;
+
+}
