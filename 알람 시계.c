7f8bc3d0ae5462766494e42e5ca59733c61d1dcc
mungon <74732825+kim-mun-gon@users.noreply.github.com>
2021-01-07 15:35:29 +0900

    2884번

---
+++ 알람 시계.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(void)
+{
+	int H, M, a;
+	
+	scanf("%d %d", &H, &M);
+	
+	if(H == 0)
+		H = 24;
+	
+	a = H * 60 + M - 45;
+	
+	H = a / 60;
+	M = a % 60;
+	
+	if(H == 24)
+		H = 0;
+	printf("%d %d", H, M);
+	
+	return 0;
+		
+}
